722813069f23f0707bb034d92df775fdd39192a1
Damian Ramirez <damianramirezalejandro@gmail.com>
2023-10-07 22:46:57 -0300

    Modificacion de componentes, menu navegable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProyectsComponent } from './pages/proyects/proyects.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/inicio', pathMatch:'full'},
-  {path:'inicio', component:DashboardComponent}
+  {path:'inicio', component:DashboardComponent},
+  {path:'projects', component:ProyectsComponent}
 ];
 
 @NgModule({
--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   showMenu = true;  // Controla la visibilidad de los botones del menú
-  
+  public listmenu:any = [{'title':'Inicio', 'icon': 'home', 'link': 'inicio'}, 
+                         {'title':'Proyectos', 'icon':'business_center', 'link': 'projects'}
+                        ];
+  public section = 'Inicio'
 }
